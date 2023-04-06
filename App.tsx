503d89b39b547069427dabd2fd413eeca46c2064
azadmohammadi-z <azadmohammadi1995@gmail.com>
2023-04-06 10:15:29 +0430

    add foants

--- App.tsx
+++ App.tsx
@@ -1,5 +1,33 @@
-import Welcome from './screens/Welcome';
+import { useCallback } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import Welcome from './src/screens/Welcome';
+import { useFonts } from 'expo-font';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
-    return <Welcome />;
+    const [fontsLoaded] = useFonts({
+        'Lato-Bold': require('./assets/fonts/Lato-Bold.ttf'),
+        'Lato-Regular': require('./assets/fonts/Lato-Regular.ttf'),
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+    return (
+        <View onLayout={onLayoutRootView} style={styles.container}>
+            <Welcome />
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
