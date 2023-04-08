496a98a6ad0b47ec7df6201e0f067a0d60d3e97b
azadmohammadi-z <azadmohammadi1995@gmail.com>
2023-04-08 12:23:46 +0430

    continue navigation and add home stack

--- src/navigators/RootStack.tsx
+++ src/navigators/RootStack.tsx
@@ -2,13 +2,16 @@ import React, { FunctionComponent } from 'react';
 
 //screen
 import Welcome from '../screens/Welcome';
+import Home from '../screens/Home';
 
 // React Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { colors } from '../components/color';
 
 export type RootStackParamList = {
-    welcome: undefined;
+    Welcome: undefined;
+    Home: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -16,8 +19,26 @@ const Stack = createStackNavigator<RootStackParamList>();
 const RootStack: FunctionComponent = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="welcome" component={Welcome} />
+            <Stack.Navigator
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: colors.graylight,
+                        borderBottomWidth: 0,
+                        shadowColor: 'transparent',
+                        shadowOpacity: 0,
+                        elevation: 0,
+                        height: 120,
+                    },
+                    headerTintColor: colors.secondary,
+                }}
+                initialRouteName="Home"
+            >
+                <Stack.Screen
+                    name="Welcome"
+                    component={Welcome}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen name="Home" component={Home} />
             </Stack.Navigator>
         </NavigationContainer>
     );
