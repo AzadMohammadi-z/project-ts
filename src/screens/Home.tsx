c39f887f67942b75cca5a60974a12c4a8bea0364
azadmohammadi-z <azadmohammadi1995@gmail.com>
2023-04-11 14:22:15 +0430

    add card section and data card to home page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,11 +2,37 @@ import React, { FunctionComponent } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { colors } from '../components/color';
 import { StatusBar } from 'expo-status-bar';
+import CardSection from '../components/Cards/CardSection';
+import logo from '../../assets/logo/logo.png';
 
 const Home: FunctionComponent = () => {
+    const cardsData = [
+        {
+            id: 1,
+            accountNo: '38457577444',
+            balance: 20000.15,
+            alias: 'Work Debit',
+            logo: logo,
+        },
+        {
+            id: 2,
+            accountNo: '38457577444',
+            balance: 20000.15,
+            alias: 'Work Debit',
+            logo: logo,
+        },
+        {
+            id: 3,
+            accountNo: '38457577444',
+            balance: 20000.15,
+            alians: 'Work Debit',
+            logo: logo,
+        },
+    ];
     return (
         <View style={styles.homeContainer}>
             <StatusBar style="dark" />
+            <CardSection data={cardsData} />
         </View>
     );
 };
